6703575ee2294e9642ad98dd36e532c24a0028d0
Ясаков Алексей <aleksey.yasakov@gazprombank.ru>
2023-05-25 21:51:40 +0400

    Добавлен пустой контроллер для проверки hello world

---
+++ src/main/java/ru/social_network/users/controller/HelloController.java
@@ -0,0 +1,19 @@
+package ru.social_network.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/")
+
+    public String index() {
+
+        return "Hello world from users service!";
+
+    }
+
+}
