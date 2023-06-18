580d78aaac75e85c383eaa5761718b7cc089e41a
Ясаков Алексей <aleksey.yasakov@gazprombank.ru>
2023-06-18 23:31:16 +0400

    Реализованы тесты для полного покрытия SubscriptionController

--- src/integrationTest/java/ru/social_network/users/test/controller/SubscriptionControllerTest.java
+++ src/integrationTest/java/ru/social_network/users/test/controller/SubscriptionControllerTest.java
@@ -3,12 +3,57 @@ package ru.social_network.users.test.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
+import ru.social_network.users.entity.Subscription;
 import ru.social_network.users.test.BaseIntegration;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class SubscriptionControllerTest extends BaseIntegration {
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка успешного получения списка подписок на пользователя")
+    void findAll() throws Exception {
+        get("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions")
+                .andExpect(content().json(
+                        """
+                                [
+                                  {
+                                    "id": "7f4d218e-f062-4169-8b68-b28626ab9244",
+                                    "userId": "291253a6-f261-11ed-a05b-0242ac120003",
+                                    "userSubscriberId": "202572d2-f261-11ed-a05b-0242ac120003",
+                                    "deleted": false,
+                                    "createTime": "2023-05-20T00:00:00",
+                                    "lastModifyTime": "2023-05-20T00:00:00"
+                                  }
+                                ]
+                                 """
+                ))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Проверка получения ошибки при запросе списка подписок на пользователя при пустой бд")
+    void findAllEmpty() throws Exception {
+        get("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions")
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"Подписки не найдены"
+                                }
+                                 """
+                ))
+                .andExpect(status().isNotFound());
+    }
 
     @Test
     @Sql(scripts = {
@@ -32,5 +77,189 @@ public class SubscriptionControllerTest extends BaseIntegration {
         assertThat(subscriptionRepository.findAll())
                 .isNotEmpty()
                 .hasSize(3);
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка получения ошибки при создании подписки на пользователя указав в теле запроса и url разный userId")
+    void subscribeNotCorrectId() throws Exception {
+        post("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions",
+                """
+                        {
+                            "userId": "291253a6-f261-11ed-a05b-0242ac120004",
+                            "userSubscriberId": "a74a7726-f261-11ed-a05b-0242ac120003",
+                            "deleted": false,
+                            "createTime": "2023-05-20T00:00:00",
+                            "lastModifyTime": "2023-05-20T00:00:00"
+                        }
+                        """)
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"id пользователя указан некорректно. id url: 291253a6-f261-11ed-a05b-0242ac120003, id в теле запроса: 291253a6-f261-11ed-a05b-0242ac120004"
+                                }
+                                 """
+                ))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка успешного обновления подписки на пользователя")
+    void update() throws Exception {
+        put("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244",
+                """
+                        {
+                          "id": "7f4d218e-f062-4169-8b68-b28626ab9244",
+                          "userId": "291253a6-f261-11ed-a05b-0242ac120003",
+                          "userSubscriberId": "a74a7726-f261-11ed-a05b-0242ac120003",
+                          "deleted": true,
+                          "createTime": "2023-05-21T00:00:00",
+                          "lastModifyTime": "2023-05-22T22:04:32"
+                        }
+                        """)
+                .andExpect(status().isOk());
+
+        Subscription actualSubscription = subscriptionRepository.findById(UUID.fromString("7f4d218e-f062-4169-8b68-b28626ab9244")).orElseThrow();
+        assertAll("actualSubscription",
+                () -> assertEquals(UUID.fromString("291253a6-f261-11ed-a05b-0242ac120003"), actualSubscription.getUserId()),
+                () -> assertEquals(UUID.fromString("a74a7726-f261-11ed-a05b-0242ac120003"), actualSubscription.getUserSubscriberId()),
+                () -> assertEquals(true, actualSubscription.getDeleted())
+        );
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка получения ошибки при обновлении подписки на пользователя с некорректным userId. Отличается в url и теле запроса")
+    void updateNotCorrectUserId() throws Exception {
+        put("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244",
+                """
+                        {
+                          "id": "7f4d218e-f062-4169-8b68-b28626ab9244",
+                          "userId": "291253a6-f261-11ed-a05b-0242ac120004",
+                          "userSubscriberId": "a74a7726-f261-11ed-a05b-0242ac120003",
+                          "deleted": true,
+                          "createTime": "2023-05-21T00:00:00",
+                          "lastModifyTime": "2023-05-22T22:04:32"
+                        }
+                        """)
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"id пользователя указан некорректно. id url: 291253a6-f261-11ed-a05b-0242ac120003, id в теле запроса: 291253a6-f261-11ed-a05b-0242ac120004"
+                                }
+                                 """
+                ))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка получения ошибки при обновлении подписки на пользователя с некорректным subscriptionsId. Отличается в url и теле запроса")
+    void updateNotCorrectSubscriptionsId() throws Exception {
+        put("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244",
+                """
+                        {
+                          "id": "7f4d218e-f062-4169-8b68-b28626ab9245",
+                          "userId": "291253a6-f261-11ed-a05b-0242ac120003",
+                          "userSubscriberId": "a74a7726-f261-11ed-a05b-0242ac120003",
+                          "deleted": true,
+                          "createTime": "2023-05-21T00:00:00",
+                          "lastModifyTime": "2023-05-22T22:04:32"
+                        }
+                        """)
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"id подписки указан некорректно. id url: 7f4d218e-f062-4169-8b68-b28626ab9244, id в теле запроса: 7f4d218e-f062-4169-8b68-b28626ab9245"
+                                }
+                                 """
+                ))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Проверка получения ошибки при обновлении подписки на пользователя с пустой бд")
+    void updateEmpty() throws Exception {
+        put("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244",
+                """
+                        {
+                          "id": "7f4d218e-f062-4169-8b68-b28626ab9244",
+                          "userId": "291253a6-f261-11ed-a05b-0242ac120003",
+                          "userSubscriberId": "a74a7726-f261-11ed-a05b-0242ac120003",
+                          "deleted": true,
+                          "createTime": "2023-05-21T00:00:00",
+                          "lastModifyTime": "2023-05-22T22:04:32"
+                        }
+                        """)
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"Подписка с id: 7f4d218e-f062-4169-8b68-b28626ab9244 - не найдена"
+                                }
+                                 """
+                ))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка успешного удаления подписки на пользователя")
+    void delete() throws Exception {
+        delete("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244")
+                .andExpect(status().isOk());
+
+        Subscription actualSubscription = subscriptionRepository.findById(UUID.fromString("7f4d218e-f062-4169-8b68-b28626ab9244")).orElseThrow();
+        assertAll("actualSubscription",
+                () -> assertEquals(true, actualSubscription.getDeleted())
+        );
+    }
+
+    @Test
+    @Sql(scripts = {
+            "classpath:sql/insert_users.sql",
+            "classpath:sql/insert_subscriptions.sql"
+    })
+    @DisplayName("Проверка успешного повторного удаления подписки на пользователя")
+    void deleteRepeat() throws Exception {
+        delete("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244")
+                .andExpect(status().isOk());
+
+        delete("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244")
+                .andExpect(status().isOk());
+
+        Subscription actualSubscription = subscriptionRepository.findById(UUID.fromString("7f4d218e-f062-4169-8b68-b28626ab9244")).orElseThrow();
+        assertAll("actualSubscription",
+                () -> assertEquals(true, actualSubscription.getDeleted())
+        );
+    }
+
+    @Test
+    @DisplayName("Проверка получения ошибки при удалении подписки на пользователя при пустой бд")
+    void deleteEmpty() throws Exception {
+        delete("/v1/users/291253a6-f261-11ed-a05b-0242ac120003/subscriptions/7f4d218e-f062-4169-8b68-b28626ab9244")
+                .andExpect(content().json(
+                        """
+                                {
+                                    "message":"Подписка с id: 7f4d218e-f062-4169-8b68-b28626ab9244 - не найдена"
+                                }
+                                 """
+                ))
+                .andExpect(status().isNotFound());
     }
 }
--- src/integrationTest/resources/sql/insert_subscriptions.sql
+++ src/integrationTest/resources/sql/insert_subscriptions.sql
@@ -4,11 +4,13 @@
 --comment Создание тестовых подписчиков
 
 -- Подписываем на Васю Никиту
-insert into subscriptions (user_id, user_subscriber_id, deleted, create_time, last_modify_time)
-values ('291253a6-f261-11ed-a05b-0242ac120003', '202572d2-f261-11ed-a05b-0242ac120003', false, '2023-05-20 00:00:00',
+insert into subscriptions (id, user_id, user_subscriber_id, deleted, create_time, last_modify_time)
+values ('7f4d218e-f062-4169-8b68-b28626ab9244', '291253a6-f261-11ed-a05b-0242ac120003',
+        '202572d2-f261-11ed-a05b-0242ac120003', false, '2023-05-20 00:00:00',
         '2023-05-20 00:00:00');
 
 -- Подписываем на Рому Васю
-insert into subscriptions (user_id, user_subscriber_id, deleted, create_time, last_modify_time)
-values ('a74a7726-f261-11ed-a05b-0242ac120003', '291253a6-f261-11ed-a05b-0242ac120003', false, '2023-05-20 00:00:00',
+insert into subscriptions (id, user_id, user_subscriber_id, deleted, create_time, last_modify_time)
+values ('15a47728-7776-4380-8786-d80c48f8184c', 'a74a7726-f261-11ed-a05b-0242ac120003',
+        '291253a6-f261-11ed-a05b-0242ac120003', false, '2023-05-20 00:00:00',
         '2023-05-20 00:00:00');
